07b8120651ec35a0958c69d52dc7e8347bff8660
obmeir <orenbm11@gmail.com>
2020-11-10 03:03:41 -0500

    fixed csv parsing in spark sql

--- Homework/4/BDM_HW4_BenMeir.py
+++ Homework/4/BDM_HW4_BenMeir.py
@@ -6,26 +6,14 @@ if __name__ == '__main__':
     sc = SparkContext()
     sqlContext = SQLContext(sc)
 
-    def parseCSV(indx, part):
-        import csv, sys
-        if indx == 0:
-            if (sys.version_info > (3, 0)): # check python version
-                next(part)
-            else:
-                part.next()
-        for p in csv.reader(part):
-            yield Row(product=p[1],date_recieved=p[0], company=p[7])
-        
-    rows = sc.textFile(sys.argv[1] if len(sys.argv)>1 else 'complaints_small.csv')\
-        .mapPartitionsWithIndex(parseCSV).cache()
-    
-    df = sqlContext.createDataFrame(rows)
+    df = sqlContext.read.csv(sys.argv[1] if len(sys.argv)>1 else 'complaints_small.csv', multiLine=True, \
+        header=True, escape="\"", inferSchema=True, lineSep="\n")
 
-    df_prodyearcomp = df.select(F.upper("product").alias("product"), F.year("date_recieved").alias("year"), F.col("Company"))\
-        .groupBy(F.col("product"), F.col("year"), F.col("Company")).count()
-        
-    # df_prodyearcomp.show()
+    # print(df.columns)
     
+    df_prodyearcomp = df.select(F.upper("Product").alias("product"), F.year("Date received").alias("year"), F.col("Company"))\
+        .groupBy(F.col("product"), F.col("year"), F.col("Company")).count()
+            
     df_report = df_prodyearcomp.groupBy(F.col("product"), F.col("year"))\
         .agg(F.sum("count").alias("Number of complaints"),\
             F.count("count").alias("Number of companies"),\
