9496f0d83096f6d982e899ac3d548ab9e2e30dad
obmeir <orenbm11@gmail.com>
2020-11-10 14:37:16 -0500

    fixed py2 analysis

--- Homework/4/BDM_HW4_BenMeir.py
+++ Homework/4/BDM_HW4_BenMeir.py
@@ -31,14 +31,19 @@ def py2_solution():
     sc = SparkContext()
     sqlContext = SQLContext(sc)
 
-    def parseCSV(indx, part):
-        import csv, datetime
-        if indx == 0:
-            part.next()
-        for p in csv.reader(part):
-            year = datetime.datetime.strptime(p[0], '%Y-%m-%d').year
-            product, company= p[1], p[7]
-            yield (product, year, company), 1
+    # def parseCSV(indx, part):
+    #     import csv, datetime
+    #     if indx == 0:
+    #         part.next()
+    #     for p in csv.reader(part):
+    #         year = datetime.datetime.strptime(p[0], '%Y-%m-%d').year
+    #         product, company= p[1], p[7]
+    #         yield (product, year, company), 1
+    
+    def map_row(row):
+        year = row["Date received"].year
+        return (row["Product"], year, row["Company"]), 1
+    
     
     def aggr_row_data(x):
         product=x[0][0].upper()
@@ -46,21 +51,28 @@ def py2_solution():
         total_complaints = sum(x[1])
         total_companies = len(x[1])
         highest_percent = round(100*max(x[1])/total_complaints)
+        return (product, year, total_complaints, total_companies, highest_percent)
 
-        return ",".join([str(x) for x in [product, year, total_complaints, total_companies, highest_percent]])
-        
+    df = sqlContext.read.csv(sys.argv[1] if len(sys.argv)>1 else 'complaints_small.csv', multiLine=True, \
+        header=True, escape="\"", inferSchema=True)\
+            .select(F.col("Date received"), F.col("Product"), F.col("Company")).cache()
 
-    rows = sc.textFile(sys.argv[1] if len(sys.argv)>1 else 'complaints_small.csv')\
-        .mapPartitionsWithIndex(parseCSV)
+    # df.show()
+    # for x in df.rdd.take(20):
+    #     print x
 
-    rdd_aggr = rows.reduceByKey(lambda x,y: x+y)\
+    rdd_complaints = df.rdd.map(map_row)
+
+    rdd_aggr = rdd_complaints.reduceByKey(lambda x,y: x+y)\
         .map(lambda x: ((x[0][0], x[0][1]), x[1]))\
-        .groupByKey().map(aggr_row_data)
+        .groupByKey().map(aggr_row_data)\
+        .sortBy(lambda x: (x[0], x[1]))\
+        .map(lambda x: ",".join(map(str,x)))
     
     rdd_result_heading = sc.parallelize(["product,year,total_complaints,total_companies,highest_percent"])
     
-    rdd_result = rdd_result_heading.union(rdd_aggr)
-    # for x in rdd_result.collect():
+    rdd_result = rdd_result_heading.union(rdd_aggr).cache()
+    # for x in rdd_result.take(20):
     #     print x
     rdd_result.saveAsTextFile(sys.argv[2] if len(sys.argv)>2 else 'report')
 
