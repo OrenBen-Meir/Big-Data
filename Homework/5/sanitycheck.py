3936e15f7beb873ea9deab7fbb8fa1dfdccc487f
obmeir <orenbm11@gmail.com>
2020-12-02 16:28:50 -0500

    expanding output

---
+++ Homework/5/sanitycheck.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+df : pd.DataFrame = pd.read_csv("oren_final_output.csv", header=None)
+print(df)
+
+print(df[[1,2,3,4,5]])
+
+df = df[[1,2,3,4,5]]
+
+df = df.sum(axis=1)
+
+s = np.sum(np.array(df))
+
+print(s)
