495cd5b778598ac327a5e71efcea068d34e1d684
obmeir <orenbm11@gmail.com>
2020-12-06 21:12:51 -0500

    rename

--- Homework/5/spark_run.sh
+++ Homework/5/spark_run.sh
@@ -3,7 +3,7 @@ spark-submit \
     --num-executors 6 \
     --executor-cores 5 \
     --executor-memory 10G \
-    BDM_FinalChallenge.py /data/share/bdm/nyc_parking_violation /data/share/bdm/nyc_cscl.csv oren_final_output
+    BDM_FinalChallenge_BenMeir.py /data/share/bdm/nyc_parking_violation /data/share/bdm/nyc_cscl.csv oren_final_output
 
 hdfs dfs -getmerge oren_final_output oren_final_output.csv
 hdfs dfs -put oren_final_output.csv oren_final_output.csv
